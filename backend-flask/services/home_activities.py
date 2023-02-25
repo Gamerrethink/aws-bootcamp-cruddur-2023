b8deeafc12beaf6e08bf687ef7c12f62bb959344
Marquis <marquismb.mb@gmail.com>
2023-02-25 19:18:53 +0000

    more troubleshooting

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,15 +1,7 @@
 from datetime import datetime, timedelta, timezone
-from opentelemetry import trace
-
-tracer = trace.get_tracer("home.activities")
-
-
 class HomeActivities:
   def run():
-    with tracer.start_as_current_span("home-activities-mock-data"):
-    span = trace.get_current_span()  
     now = datetime.now(timezone.utc).astimezone()
-    span.set_attribute("app.now", now.isoformat())
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -49,5 +41,4 @@ class HomeActivities:
       'replies': []
     }
     ]
-    span.set_attribute("app.result_length", len(results))
     return results
