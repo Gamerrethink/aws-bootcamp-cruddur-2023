1adb32f3be0d84867703c5c78a4fd0872d2dd4aa
Marquis <marquismb.mb@gmail.com>
2023-02-25 18:40:14 +0000

    add spans and attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,7 +7,9 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
+    span = trace.get_current_span()  
     now = datetime.now(timezone.utc).astimezone()
+    span.set_attribute("app.now", now.isoformat())
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -47,4 +49,5 @@ class HomeActivities:
       'replies': []
     }
     ]
+    span.set_attribute("app.result_length", len(results))
     return results
