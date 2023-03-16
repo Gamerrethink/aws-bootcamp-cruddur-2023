32613676c091af1116933f46359448f8ad274494
Marquis <marquismb.mb@gmail.com>
2023-03-16 20:15:05 +0000

    cognito lambda 3-16-2023

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -5,15 +5,16 @@ DROP TABLE IF EXISTS public.users cascade;
 DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
-  uuid UUID default uuid_generate_v4() primary key,
-  display_name text,
-  handle text,
-  cognito_user_id text,
+  uuid UUID default uuid_generate_v4() PRIMARY KEY,
+  display_name text NOT NULL,
+  handle text NOT NULL,
+  email text NOT NULL,
+  cognito_user_id text NOT NULL,
   created_at timestamp default current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
-  uuid UUID default uuid_generate_v4() primary key,
+  uuid UUID default uuid_generate_v4() PRIMARY KEY,
   user_uuid UUID REFERENCES public.users(uuid) NOT NULL,
   message text NOT NULL,
   replies_count integer default 0,
