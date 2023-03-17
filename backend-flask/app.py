918ceba54a586bcaee0dde8c9b3c1d4ccf964863
Marquis <marquismb.mb@gmail.com>
2023-03-17 19:15:36 +0000

    created activities

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -201,7 +201,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'awsbootcamp'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,42 +1,17 @@
 from datetime import datetime, timedelta, timezone
-#Honeycomb
-from opentelemetry import trace 
-# pyscopg
-from lib.db import pool, query_wrap_object, query_wrap_array
+from opentelemetry import trace
 
-#Honeycomb
-tracer = trace.get_tracer("home.activities")
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
-    #logger.info("HomeActivities") #CloudWatch logs
-    with tracer.start_as_current_span("home-activities-custom-span-mock-data"):  #Honeycomb custom span
-      span = trace.get_current_span() #Honeycomb attribute
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat()) #Honeycomb attribute
-      
-      sql = query_wrap_array("""
-      SELECT
-        activities.uuid,
-        users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
-      FROM public.activities
-      LEFT JOIN public.users ON users.uuid = activities.user_uuid
-      ORDER BY activities.created_at DESC
-      """)
-      #span.set_attribute("app.result_length",len(results)) #Honeycomb 
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()   
-      return json[0]
-      return results
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
