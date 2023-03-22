e0551983f516b8a67a14a0b71516b34f44f59ec6
Marquis <marquismb.mb@gmail.com>
2023-03-22 23:10:55 +0000

    successful implementation of backend

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,11 +2,11 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Marquis Brown' , 'marquismb.mb@gmail.com' , 'awscloudcamp' , 'MOCK');
+  ('Marquis Brown' , 'marquismb.mb@gmail.com' , 'awsbootcamp' , 'MOCK');
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'awscloudcamp' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'awsbootcamp' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
