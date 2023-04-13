4149b63d5dbdff43ff6a91179ce255b30ea896fd
Marquis <marquismb.mb@gmail.com>
2023-04-13 21:25:10 +0000

    37 minutes left in avatar upload video

--- aws/lambdas/upload-avatars-cruddr/function.rb
+++ aws/lambdas/upload-avatars-cruddr/function.rb
@@ -1,8 +1,15 @@
 require 'aws-sdk-s3'
+require 'json'
 
-client = Aws::S3::Client.new
+def handler(event:, context:)
+  puts event
+  s3 = Aws::S3::Resource.new
+  bucket_name = ENV["UPLOADS_BUCKET_NAME"]
+  object_key = 'mock.jpg'
 
-signer = Aws::S3::Presigner.new
-url, headers = signer.presigned_request(
-  :get_object, bucket: "bucket", key: "key"
-)
+  obj = s3.bucket(bucket_name).object(object_key)
+  url = obj.presigned_url(:put, expires_in: 60 * 5)
+  url # this is the data that will be returned
+  body = {url: url}.to_json
+  { statusCode: 200, body: body }
+end
--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -14,7 +14,27 @@ export default function ProfileForm(props) {
   }, [props.profile])
 
   const s3upload = async (event)=> {
-
+    try {
+      console.log('s3upload')
+      const backend_url = "https://xdlb2qo8vb.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      const res = await fetch(backend_url, {
+        method: "POST",
+        headers: {
+          'Authorization': `Bearer ${access_token}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }})
+      let data = await res.json();
+      if (res.status === 200) {
+       console.log('presigned url',data)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   const onsubmit = async (event) => {
