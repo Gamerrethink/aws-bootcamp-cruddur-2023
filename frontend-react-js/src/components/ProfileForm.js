39ee6c53a25a89a46e70ff35afdc54c04b2a59b4
Marquis <marquismb.mb@gmail.com>
2023-04-14 14:58:10 +0000

    almost fix cors issue

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -25,10 +25,11 @@ export default function ProfileForm(props) {
           'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
-        }})
+      }})
       let data = await res.json();
       if (res.status === 200) {
-       console.log('presigned url',data)
+        console.log('presigned url',data)
+        return data.url
       } else {
         console.log(res)
       }
@@ -45,19 +46,19 @@ export default function ProfileForm(props) {
     const type = file.type
     const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
-
+    const presignedurl = await s3uploadkey()
+    console.log('pp',presignedurl)
     try {
       console.log('s3upload')
-      const backend_url = ""
-      const res = await fetch(backend_url, {
+      const res = await fetch(presignedurl, {
         method: "PUT",
         body: file,
         headers: {
           'Content-Type': type
-        }})
+      }})
       let data = await res.json();
       if (res.status === 200) {
-       console.log('presigned url',data)
+        setPresignedurl(data.url)
       } else {
         console.log(res)
       }
@@ -65,6 +66,7 @@ export default function ProfileForm(props) {
       console.log(err);
     }
   }
+
   const onsubmit = async (event) => {
     event.preventDefault();
     try {
@@ -124,11 +126,8 @@ export default function ProfileForm(props) {
             </div>
           </div>
           <div className="popup_content">
-
-            <div className="upload" onClick={s3uploadkey}>
-              Upload Avatar
-            </div>
-          <input type ="file" name="avatarupload" onChange={s3upload} />
+            
+          <input type="file" name="avatarupload" onChange={s3upload} />
 
             <div className="field display_name">
               <label>Display Name</label>
