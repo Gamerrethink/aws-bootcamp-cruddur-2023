03101f301f3a29cd2b939ad51c9ea1822ff0e306
Marquis <marquismb.mb@gmail.com>
2023-04-15 17:31:02 +0000

    create buildspec file

--- aws/lambdas/upload-avatars-cruddr/function.rb
+++ aws/lambdas/upload-avatars-cruddr/function.rb
@@ -14,7 +14,7 @@ def handler(event:, context:)
   { 
     headers: {
       "Access-Control-Allow-Headers": "*, Authorization",
-      "Access-Control-Allow-Origin": "https://3000-gamerrethin-awsbootcamp-p6qyvwopcy9.ws-us94.gitpod.io",
+      "Access-Control-Allow-Origin": "https://3000-gamerrethin-awsbootcamp-2qnkho9aliv.ws-us94.gitpod.io",
       "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
     },
     statusCode: 200, 
