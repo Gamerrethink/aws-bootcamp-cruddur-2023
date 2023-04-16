67d7bd44dd9a09b2189e309044e5dd8f8b82fb60
Marquis <marquismb.mb@gmail.com>
2023-04-16 23:59:41 +0000

    fully done with week 8!

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -12,14 +12,18 @@ export default function ProfileForm(props) {
     setDisplayName(props.profile.display_name);
   }, [props.profile])
 
-  const s3uploadkey = async (event)=> {
+  const s3uploadkey = async (extension)=> {
+    console.log('ext',extension)
     try {
-      console.log('s3upload')
       const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
+      const json = {
+        extension: extension
+      }
       const res = await fetch(gateway_url, {
         method: "POST",
+        body: JSON.stringify(json),
         headers: {
           'Origin': process.env.REACT_APP_FRONTEND_URL,
           'Authorization': `Bearer ${access_token}`,
@@ -28,7 +32,6 @@ export default function ProfileForm(props) {
       }})
       let data = await res.json();
       if (res.status === 200) {
-        console.log('presigned url',data)
         return data.url
       } else {
         console.log(res)
@@ -40,14 +43,14 @@ export default function ProfileForm(props) {
   const s3upload = async (event)=> {
     console.log('event',event)
     const file = event.target.files[0]
-    console.log('file',file)
     const filename = file.name
     const size = file.size
     const type = file.type
     const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
-    const presignedurl = await s3uploadkey()
-    console.log('pp',presignedurl)
+    const fileparts = filename.split('.')
+    const extension = fileparts[fileparts.length-1]
+    const presignedurl = await s3uploadkey(extension)
     try {
       console.log('s3upload')
       const res = await fetch(presignedurl, {
@@ -55,10 +58,10 @@ export default function ProfileForm(props) {
         body: file,
         headers: {
           'Content-Type': type
-      }})
-      let data = await res.json();
+      }
+    })
       if (res.status === 200) {
-        setPresignedurl(data.url)
+
       } else {
         console.log(res)
       }
